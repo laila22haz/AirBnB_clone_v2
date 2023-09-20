1b9efed8f1cefe8eb16696f9170dee53763c563c
laila22haz <hazmirlaila316@gmail.com>
2023-09-20 22:49:17 +0100

    fix place amenity 2

--- models/amenity.py
+++ models/amenity.py
@@ -13,8 +13,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if storage_data == 'db':
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place',
-                                       secondary="place_amenity",
-                                       back_populates="amenities")
+        place_amenities = relationship('Place', secondary="place_amenity",
+                back_populates="amenities")
     else:
         name = ""
