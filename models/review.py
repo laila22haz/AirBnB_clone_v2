0a22d9c813b62e735927c0df5ed4add693e8b549
Fatimazahraker <fatimazahraker>
2023-09-20 21:57:36 +0000

    task

--- models/review.py
+++ models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Text
 from os import getenv
 
 storage_data = getenv("HBNB_TYPE_STORAGE")
@@ -12,7 +12,7 @@ class Review(BaseModel, Base):
 
     __tablename__ = "reviews"
     if storage_data == "db":
-        text = Column(String(1024), nullable=False)
+        text = Column(Text:wq(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     else:
