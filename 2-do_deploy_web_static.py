219c189c308baa1c2d90ac66b01503ee6da79d48
laila22haz <hazmirlaila316@gmail.com>
2023-10-05 21:48:28 +0100

    2-do_deploy_web_static.py fix the pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,18 +8,23 @@ import os
 
 env.hosts = ['ubuntu@54.226.32.212', '52.201.179.163']
 
+
 def do_deploy(archive_path):
     if not files.exists(archive_path):
         return False
     try:
         archive_name = os.path.basename(archive_path)
         archive_ext = os.path.splitext(archive_name)[0]
-        new_folder = '/data/web_static/releases/'.format(archive_ext)
+        new_folder = '/data/web_static/\
+            releases/'.format(archive_ext)
         put(archive_name, '/temp')
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(archive_name, archive_ext))
+        run('tar -xzf /tmp/{} -C /data/web_static\
+        /releases/{}'.format(archive_name, archive_ext))
         run('rm -rf /temp/{}'.format(archive_name))
         run('rm -rf {}'.format('/data/web_static/current'))
-        run('ln -s /data/web_static/releases/{}/' '/data/web_static/current'.format(archive_ext))
+        run('ln -s /data/web_static/releases\
+            /{}/' '/data/web_static/current\
+                '.format(archive_ext))
         print("New version deployed!")
         return True
     except Exception as e:
