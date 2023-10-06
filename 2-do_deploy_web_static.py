2ebd328694b38b73a71f2f58acd1f161da4065da
laila22haz <hazmirlaila316@gmail.com>
2023-10-06 02:23:24 +0100

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-"""  script (based on the file 1-pack_web_static.py"""
-
+"""Deploy archive"""
 
 from fabric.api import *
-import os
+from os.path import exists, splitext, basename
 
-env.user = 'ubuntu'
-env.hosts = ['ubuntu@54.226.32.212', '52.201.179.163']
+env.hosts = ['100.26.9.167', '52.201.179.163']
 
 
 @task
 def do_deploy(archive_path):
-    """ method to deploy"""
-    try:
-        if not local("test -e {}".format(archive_path)).succeeded:
-            return False
-        file_withex = os.path.basename(archive_path)
-        file_noex, ex = os.path.splitext(file_withex)
-        put(archive_path, "/tmp/")
-        run("rm -f {}".format(archive_path))
-        run("mkdir -p /data/web_static/releases/{}/".format(file_noex))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_withex, file_noex))
-        run("rm /tmp/{}".format(file_withex))
-        run("mv /data/web_static/releases/{0}/web_static/* "
-            "/data/web_static/releases/{0}/".format(file_noex))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(file_noex))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current".format(file_noex))
-        print("New version deployed!")
-        return True
-    except Exception:
-        return False
+        """Fabric script that distributes an archive to your web servers"""
+        if not exists(archive_path):
+                return False
+        try:
+                archive_name = basename(archive_path)
+                archive_ext = splitext(archive_name)[0]
+                tmp_path = "/tmp/{}".format(archive_name)
+                data_path = "/data/web_static/releases/{}".format(archive_ext)
+                # Fabric commands
+                put(archive_path, '/tmp/')
+                run('mkdir -p {}'.format(data_path))
+                run('tar -xzf {}  -C {}'.format(tmp_path, data_path))
+                run('rm {}'.format(tmp_path))
+                run('mv {}/web_static/* {}/'.format(data_path, data_path))
+                run('rm -rf {}/web_static'.format(data_path))
+                run('rm -rf /data/web_static/current')
+                run('ln -s {} /data/web_static/current'.format(data_path))
+                print("New version deployed!")
+                return True
+        except Exception as e:
+                return False
