f8ce0fdb20d88ecc49236c4c688903d62f96d357
laila22haz <hazmirlaila316@gmail.com>
2023-10-20 12:19:54 +0100

    helle flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,8 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes = False)
+@app.route("/", strict_slashes=False)
 def hello_world():
+    """
+    This function handles the root URL of the Flask application.
+
+    Returns:
+        str: A simple greeting message.
+    """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
