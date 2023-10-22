d7c8c058469afe806f08ace5415d6c9b26c52b81
laila22haz <hazmirlaila316@gmail.com>
2023-10-22 23:33:48 +0100

    flask/city_state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-""" Starts a Flask web application"""""
+"""
+This module defines Flask application.
+"""
 
-from flask import *
+from flask import Flask, app, render_template
 from models import storage
 from models.state import State
-from models.city import City
+
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
 @app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    cities = storage.all(City).values()
-    sorted_cities = sorted(cities, key=lambda city: city.name)
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html',
-                           cities=sorted_cities, states=sorted_states)
+def states_list():
+    """
+    This function handles the root URL of the Flask application.
+
+    Returns:
+        str: HTML page with list of city.
+    """
+    states = sorted(storage.all(State).values(), key=lambda i: i.name)
+    return render_template('8-cities_by_states.html', states=states)
+
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """close Session"""
+def teardown_db(exception=None):
+    """close session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    """ Makes the app run when called from the command line"""
     app.run(host='0.0.0.0', port=5000)
