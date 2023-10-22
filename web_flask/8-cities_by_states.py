e952b88333bd132f88138bd8e0ada44492b36b8a
laila22haz <hazmirlaila316@gmail.com>
2023-10-22 20:58:03 +0100

    flask/cities_by_states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines Flask application.
+"""
+
+from flask import Flask, app, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def states_list():
+    """
+    This function handles the root URL of the Flask application.
+
+    Returns:
+        str: HTML page with list of Cities by states.
+    """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def after_request(response):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
