49a7fd285ae5fa6d0174c83bd8d1171c240688bd
laila22haz <hazmirlaila316@gmail.com>
2023-10-25 17:54:51 +0100

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""
-This module defines Flask application.
-"""
+""" Starts a Flask web application"""""
 
-from flask import Flask, app, render_template
-from models import *
+from flask import *
+from models import storage
 from models.state import State
-
+from models.city import City
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-def states_list():
-    """
-    This function handles the root URL of the Flask application.
-
-    Returns:
-        str: HTML page with list of city.
-    """
-    states = sorted(storage.all(State).values(), key=lambda i: i.name)
+def cities_by_states():
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
-    """close session"""
+def teardown_db(self):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == '__main__':
+    """ Makes the app run when called from the command line"""
     app.run(host='0.0.0.0', port=5000)
